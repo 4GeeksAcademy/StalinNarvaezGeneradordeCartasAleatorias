c43f184106ea502abbebc05d8cfadac697593525
Stalin Narváez <108230261+StalinNarvaez@users.noreply.github.com>
2023-04-28 14:20:47 +0000

    Generador de cartas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,42 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const pinta = ["♦", "♥", "♠", "♣"];
+  const digito = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  function nuevaCarta() {
+    let cartaPinta = pinta[Math.floor(Math.random() * pinta.length)];
+    let cartaDigito = digito[Math.floor(Math.random() * digito.length)];
+
+    document.getElementById("valor").innerHTML = cartaDigito;
+    let paloSuperior = (document.getElementById("palo").innerHTML = cartaPinta);
+    document.getElementById("palo2").innerHTML = paloSuperior;
+
+    if (cartaPinta === "♦" || cartaPinta === "♥") {
+      document.getElementById("palo").style.color = "red";
+      document.getElementById("palo").style.fontSize = "80px";
+    } else {
+      document.getElementById("palo").style.color = "black";
+      document.getElementById("palo").style.fontSize = "80px";
+    }
+    if (cartaPinta === "♦" || cartaPinta === "♥") {
+      document.getElementById("palo2").style.color = "red";
+      document.getElementById("palo2").style.fontSize = "80px";
+    } else {
+      document.getElementById("palo2").style.color = "black";
+      document.getElementById("palo2").style.fontSize = "80px";
+    }
+  }
+  document.getElementById("boton").onclick = function() {
+    nuevaCarta();
+  };
+  const intervalId = setInterval(nuevaCarta, 10000);
+  function redimensionarCarta() {
+    let ancho = document.getElementById("ancho").value;
+    let alto = document.getElementById("alto").value;
+    document.getElementById("contenidoCarta").style.width = ancho + "px";
+    document.getElementById("contenidoCarta").style.height = alto + "px";
+  }
+  document.getElementById("botonRedimensionar").onclick = function() {
+    redimensionarCarta();
+  };
 };
